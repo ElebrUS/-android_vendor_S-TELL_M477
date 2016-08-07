c9f5ab3dd284f62ad07e332f5c53b551900ba57a
ElebrUS <megaelebrus@ya.ru>
2016-08-07 23:31:42 +0300

    first commit

---
+++ M477-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/S-TELL/M477/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/S-TELL/M477/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/S-TELL/M477/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/S-TELL/M477/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/S-TELL/M477/xbin,system/xbin)
+
